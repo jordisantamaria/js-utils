483e12b125835aa81b1376dee8d9e51095c9ec7c
jordisantamaria <jordisantamaria1a@gmail.com>
2022-11-29 11:13:32 +0900

    refactor chat with mentions

--- src/strings/chatWithMentions.ts
+++ src/strings/chatWithMentions.ts
@@ -3,14 +3,17 @@ type Member = {
   accountId: number
 }
 
-export const getTextWithMentionsList = (text: string, members: Member[]) => {
-  return formatText(text, members, [])
+export const getTextWithMentionsList = (
+  text: string,
+  members: Member[]
+) => {
+  return formatMentionText(text, members, [])
 }
 
-const formatText = (
+const formatMentionText = (
   text: string,
   members: Member[],
-  resultList: any[],
+  resultList: any[]
 ): any[] => {
   const index = text.indexOf('@')
   if (index < 0) {
@@ -18,22 +21,33 @@ const formatText = (
     return text.length > 0 ? [...resultList, { text: text }] : resultList
   }
   const list = [...resultList]
-  if (index > 0) {
-    // include text before mention
-    list.push({ text: text.substring(0, index) })
-  }
-  // text starting by @
-  let surplusText = text.substring(index)
 
+  let foundMember: any = null
   members.forEach((member) => {
-    const mentionIndex = surplusText.indexOf(member.title!)
-    if (mentionIndex === 1) {
-      // include mention text
-      list.push({ text: `@${member.title}`, accountId: member.accountId })
-      // text after mention
-      surplusText = surplusText.substring(member.title.length + 1)
+    const mentionIndex = text.indexOf(member.title!)
+    if (mentionIndex === index + 1) {
+      foundMember = { text: `@${member.title}`, accountId: member.accountId }
     }
   })
-  return formatText(surplusText, members, list)
-}
 
+  let surplusText = text
+  if (foundMember) {
+    if (index > 0) {
+      // include text before mention
+      list.push({ text: text.substring(0, index) })
+    }
+    // include mention
+    list.push(foundMember)
+    surplusText = surplusText.substring(index + foundMember.text.length)
+  } else {
+    const indexNextMention = text.substring(index + 1).indexOf('@')
+    if (indexNextMention < 0) {
+      // if not more mentions, return text formated and include rest of text after last mention
+      return text.length > 0 ? [...resultList, { text: text }] : resultList
+    }
+    // if not found, add text before next mention
+    list.push({ text: text.substring(0, index + 1 + indexNextMention) })
+    surplusText = surplusText.substring(index + 1 + indexNextMention)
+  }
+  return formatMentionText(surplusText, members, list)
+}
--- tests/getTextWithMentionsList.test.ts
+++ tests/getTextWithMentionsList.test.ts
@@ -1,17 +1,16 @@
 import { getTextWithMentionsList } from '../src/strings/chatWithMentions'
 
-const members = [
-  { title: 'jordi santamaria', accountId: 1 },
-  { title: 'a random member', accountId: 2 },
-]
-
 test('text with mentions', () => {
+  const members: any[] = [
+    { title: 'member one', accountId: 1 },
+    { title: 'a random member', accountId: 2 },
+  ]
   const text =
-    'Hello @jordi santamaria how are you, and  @a random member also is here'
+    'Hello @member one how are you, and  @a random member also is here'
   const textWithMentions = getTextWithMentionsList(text, members)
   expect(textWithMentions).toEqual([
     { text: 'Hello ' },
-    { text: '@jordi santamaria', accountId: 1 },
+    { text: '@member one', accountId: 1 },
     { text: ' how are you, and  ' },
     { text: '@a random member', accountId: 2 },
     { text: ' also is here' },
@@ -19,29 +18,53 @@ test('text with mentions', () => {
 })
 
 test('text with mentions ending with mention', () => {
-  const text = 'Hello @jordi santamaria how are you, and  @a random member'
+  const members: any[] = [
+    { title: 'member one', accountId: 1 },
+    { title: 'a random member', accountId: 2 },
+  ]
+  const text = 'Hello @member one how are you, and  @a random member'
   const textWithMentions = getTextWithMentionsList(text, members)
   expect(textWithMentions).toEqual([
     { text: 'Hello ' },
-    { text: '@jordi santamaria', accountId: 1 },
+    { text: '@member one', accountId: 1 },
     { text: ' how are you, and  ' },
     { text: '@a random member', accountId: 2 },
   ])
 })
 
 test('text with mentions start with mention', () => {
-  const text = '@jordi santamaria how are you, and  @a random member'
+  const members: any[] = [
+    { title: 'member one', accountId: 1 },
+    { title: 'a random member', accountId: 2 },
+  ]
+  const text = '@member one how are you, and  @a random member'
   const textWithMentions = getTextWithMentionsList(text, members)
   expect(textWithMentions).toEqual([
-    { text: '@jordi santamaria', accountId: 1 },
+    { text: '@member one', accountId: 1 },
     { text: ' how are you, and  ' },
     { text: '@a random member', accountId: 2 },
   ])
 })
-test('text with mentions start with mention', () => {
-  const text = '@jordi santamaria'
+
+test('text with mention not existing member', () => {
+  const members: any[] = [
+    { title: 'member one', accountId: 1 },
+    { title: 'a random member', accountId: 2 },
+  ]
+  const text = 'hello @jo how are you'
+  const textWithMentions = getTextWithMentionsList(text, members)
+  expect(textWithMentions).toEqual([{ text: 'hello @jo how are you' }])
+})
+test('text with mention not existing member and existing member', () => {
+  const members: any[] = [
+    { title: 'member one', accountId: 1 },
+    { title: 'a random member', accountId: 2 },
+  ]
+  const text = 'hello @jo how are you, and @member one also'
   const textWithMentions = getTextWithMentionsList(text, members)
   expect(textWithMentions).toEqual([
-    { text: '@jordi santamaria', accountId: 1 },
+    { text: 'hello @jo how are you, and ' },
+    { text: '@member one', accountId: 1 },
+    { text: ' also' },
   ])
 })
